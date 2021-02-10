1563ad493f4aa5b77b902db135ff754f82e72a8b
shumiole <alana.linch@gmail.com>
2021-02-10 22:46:17 +0200

    input added

--- SemWork.py
+++ SemWork.py
@@ -1,1 +1,12 @@
-print('henlo')
+import sys
+mode = input("Please, choose mode:\n"
+             "1. Encrypt message to PNM P4 picture.\n"
+             "2. Decrypt message from PNM P4 picture.\n"
+             "[1/2]?\t")
+
+try:
+    assert (mode == '1' or mode == '2')
+except AssertionError as ex:
+    print("Invalid input.")
+    sys.exit()
+
