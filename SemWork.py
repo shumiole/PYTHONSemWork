92a288d0b8a82fbd52f50f45c69ec296aaf3b087
shumiole <alana.linch@gmail.com>
2021-02-10 22:56:44 +0200

    programm structure

--- SemWork.py
+++ SemWork.py
@@ -1,12 +1,31 @@
 import sys
+
+
+def verify_input(input_str):
+    try:
+        assert (input_str == '1' or input_str == '2')
+    except AssertionError:
+        print("Invalid input.")
+        sys.exit()
+
+
+def encrypt():
+    pass
+
+
+def decrypt():
+    pass
+
+
 mode = input("Please, choose mode:\n"
              "1. Encrypt message to PNM P4 picture.\n"
              "2. Decrypt message from PNM P4 picture.\n"
              "[1/2]?\t")
 
-try:
-    assert (mode == '1' or mode == '2')
-except AssertionError as ex:
-    print("Invalid input.")
-    sys.exit()
+verify_input(mode)
+
+if mode == '1':
+    encrypt()
+else:
+    decrypt()
 
